6fc44c4524a35759110ba05bd6cad3364e0c1fd7
pixcc <darkilyakill@gmail.com>
2021-02-27 05:17:31 +0300

    Added Fibonacci Method

--- Lab1/src/FibonacciMethod.java
+++ Lab1/src/FibonacciMethod.java
@@ -2,13 +2,43 @@ import java.util.function.Function;
 
 public class FibonacciMethod extends AbstractMethod {
 
-
     protected FibonacciMethod(double eps, double start, double end, Function<Double, Double> f) {
         super(eps, start, end, f);
     }
 
     @Override
     public double findMin() {
-        return 0;
+        int n = (int) (Math.log(Math.ceil(Math.sqrt(5) * (end - start) / eps)) / Math.log((1 + Math.sqrt(5)) / 2.0) + 1);
+        int[] Fib = new int[n + 1];
+        Fib[0] = 0;
+        Fib[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            Fib[i] = Fib[i - 2] + Fib[i - 1];
+        }
+
+        double left = start;
+        double right = end;
+        double x1 = left + Fib[n - 2] * (right - left) / Fib[n];
+        double x2 = left + Fib[n - 1] * (right - left) / Fib[n];
+        double f1 = f.apply(x1);
+        double f2 = f.apply(x2);
+        for (int k = 1; k <= n - 2; k++) {
+            if (f1 > f2) {
+                left = x1;
+                x1 = x2;
+                x2 = left + Fib[n - k - 1] * (right - left) / Fib[n - k];
+                f1 = f2;
+                f2 = f.apply(x2);
+            } else {
+                right = x2;
+                x2 = x1;
+                x1 = left + Fib[n - k - 2] * (right - left) / Fib[n - k];
+                f2 = f1;
+                f1 = f.apply(x1);
+            }
+        }
+
+        return (x1 + x2) / 2.0;
     }
 }
--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -3,11 +3,15 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
         Function<Double, Double> f = x -> Math.pow(Math.log10(x - 2), 2) + Math.pow(Math.log10(10 - x), 2) - Math.pow(x, 0.2);
-        OptimizationMethod method = new DichotomyMethod(1e-6, 6, 9.9, f, 1e-6);
-        System.out.println(method.findMin());
+
+        OptimizationMethod dichotomyMethod = new DichotomyMethod(1e-6, 6, 9.9, f, 1e-6);
+        System.out.println(dichotomyMethod.findMin());
 
         OptimizationMethod goldenMethod = new GoldenRatioMethod(1e-6, 6, 9.9, f);
         System.out.println(goldenMethod.findMin());
+
+        OptimizationMethod fibonacciMethod = new FibonacciMethod(1e-9, 6, 9.9, f);
+        System.out.println(fibonacciMethod.findMin());
 
     }
 }
