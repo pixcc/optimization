6e702fd689c3eec73523ecb368672bc5115db037
pixcc <darkilyakill@gmail.com>
2021-03-04 19:14:47 +0300

    minor constructor fix in fibonacci

--- Lab1/src/FibonacciMethod.java
+++ Lab1/src/FibonacciMethod.java
@@ -3,7 +3,7 @@ import java.util.function.Function;
 
 public class FibonacciMethod extends AbstractMethod {
 
-    protected FibonacciMethod(String name, double eps, double start, double end, Function<Double, Double> f) {
+    public FibonacciMethod(String name, double eps, double start, double end, Function<Double, Double> f) {
         super(name, eps, start, end, f);
     }
 
