87a53b9601eda8ed5a4afba0a2521fac894fbbbb
Maksim <maksgord-152@yandex.ru>
2021-03-13 11:56:33 +0300

    javafx init added

---
+++ Lab1/src/main/java/ui/MainApp.java
@@ -0,0 +1,28 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("Methods visualization");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
