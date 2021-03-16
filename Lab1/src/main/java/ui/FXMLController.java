5a643f89ec66e7954a5002587c7d3e599bce896e
Maksim <maksgord-152@yandex.ru>
2021-03-16 17:00:36 +0300

    several fixes

--- Lab1/src/main/java/ui/FXMLController.java
+++ Lab1/src/main/java/ui/FXMLController.java
@@ -3,10 +3,13 @@ package ui;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import methods.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -14,6 +17,15 @@ import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
 public class FXMLController {
+
+    @FXML
+    private Label statisticsLabel;
+
+    @FXML
+    private NumberAxis yAxis;
+
+    @FXML
+    private NumberAxis xAxis;
 
     @FXML
     private LineChart<Double, Double> chart;
@@ -30,19 +42,28 @@ public class FXMLController {
 
     public void initialize() {
         TestData testData = testMapping.get(selectedTestBox.getValue());
-        OptimizationMethod optimizationMethod = getOptimizationMethod(testData);
 
-        if (optimizationMethod instanceof ParabolicMethod) {
-            ParabolicMethod parabolicMethod = (ParabolicMethod) optimizationMethod;
-            for (Parabola parabola : parabolicMethod.getIntermediateParabolas()) {
-                showFunctionSegment(x -> parabola.getA() * x * x + parabola.getB() * x + parabola.getC(), testData.getSegment());
-            }
-        } else {
-            for (Segment segment : optimizationMethod.getIntermediateSegments()) {
-                showFunctionSegment(testData.getF(), segment);
-            }
-        }
+        xAxis.setAutoRanging(false);
+        xAxis.setTickUnit(0.5);
+        xAxis.setLowerBound(testData.getSegment().getLeft());
+        xAxis.setUpperBound(testData.getSegment().getRight());
+
+        OptimizationMethod optimizationMethod = getOptimizationMethod(testData);
+        double minPoint = optimizationMethod.findMin();
+
+        List<Segment> segmentList = optimizationMethod.getIntermediateSegments();
+        List<XYChart.Series<Double, Double>> series = prepareSeries(testData.getF(), segmentList);
+        chart.getData().addAll(series);
+
+
+        statisticsLabel.setText(String.format("""
+                Minimum point: %f
+                Minimum value: %f
+                Iterations: %d
+                
+                """, minPoint,testData.getF().apply(minPoint), segmentList.size()));
     }
+
 
     @FXML
     public void buttonClicked(Event e) {
@@ -50,14 +71,27 @@ public class FXMLController {
         initialize();
     }
 
-    private List<XYChart.Data<Double, Double>> calculateFunctionOnSegment(Function<Double, Double> f, Segment segment, double step) {
-        return DoubleStream.iterate(segment.getLeft(), x -> x <= segment.getRight(), x -> x + step).parallel().boxed().map(x -> new XYChart.Data<>(x, f.apply(x))).collect(Collectors.toList());
+    private List<XYChart.Data<Double, Double>> calculateFunctionOnSegment(Function<Double, Double> f, Segment segment) {
+        return DoubleStream.iterate(segment.getLeft(), x -> x <= segment.getRight(), x -> x + 0.001).parallel().boxed().map(x -> new XYChart.Data<>(x, f.apply(x))).collect(Collectors.toList());
     }
 
-    private void showFunctionSegment(Function<Double, Double> f, Segment segment) {
-        XYChart.Series<Double, Double> series = new XYChart.Series<>();
-        series.getData().addAll(calculateFunctionOnSegment(f, segment, 0.01));
-        chart.getData().add(series);
+    private List<XYChart.Series<Double, Double>> prepareSeries(Function<Double, Double> f, List<Segment> segments) {
+        List<XYChart.Series<Double, Double>> seriesList = new ArrayList<>();
+        for (int i = 0; i + 1 < segments.size(); ++i) {
+            XYChart.Series<Double, Double> series = new XYChart.Series<>();
+            Segment left = new Segment(segments.get(i).getLeft(), segments.get(i + 1).getLeft());
+            Segment right = new Segment(segments.get(i + 1).getRight(), segments.get(i).getRight());
+            if (left.getLength() >= 1e-9) {
+                series.getData().addAll(calculateFunctionOnSegment(f, left));
+            }
+            if (right.getLength() >= 1e-9) {
+                series.getData().addAll(calculateFunctionOnSegment(f, right));
+            }
+            if (!series.getData().isEmpty()) {
+                seriesList.add(series);
+            }
+        }
+        return seriesList;
     }
 
     private OptimizationMethod getOptimizationMethod(TestData testData) {
@@ -69,7 +103,7 @@ public class FXMLController {
             return new DichotomyMethod("Метод дихотомии", 1e-6, left, right, f, 1e-6);
         } else if ("Golden ratio method".equals(s)) {
             return new GoldenRatioMethod("Метод золотого сечения", 1e-6, left, right, f);
-        } else if ("FibonacciMethod".equals(s)) {
+        } else if ("Fibonacci method".equals(s)) {
             return new FibonacciMethod("Метод фибоначи", 1e-6, left, right, f);
         } else if ("Successive parabolic method".equals(s)) {
             return new SuccessiveParabolicMethod("Метод парабол", 1e-6, left, right, f);
